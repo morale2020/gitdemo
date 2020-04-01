d788135d001f8ca01a7d6ac189eb0a6c800a3d83
mac <morale@macdeMac.local>
2020-04-01 23:17:19 +0800

    update fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -4,3 +4,16 @@ Created on Thu May 30 15:52:02 2019
 
 @author: 18420
 """
+
+
+import tkinter
+from tkinter import Menu
+from tkinter import scrolledtext
+import tkinter.messagebox
+
+import re
+import os
+
+from pylab import mpl
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+import matplotlib.pyplot as plt
