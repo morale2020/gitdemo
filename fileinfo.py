16e230e1cbcf67eade1f5b6080d26ad47cf7e0f6
morale <morale@gmail.com>
2020-04-01 23:24:31 +0800

    update fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -17,3 +17,12 @@ import os
 from pylab import mpl
 mpl.rcParams['font.sans-serif'] = ['SimHei']
 import matplotlib.pyplot as plt
+
+#读取文本文件
+f = open('英语作文.txt')
+ff = f.read()
+l1 = []
+line = ff.replace('.',' ').replace('!',' ').replace(',',' ').replace('?',' ').replace('  ',' ').replace(';',' ').replace("’",' ').replace("“",' ').replace("”",' ')
+w = line.split()
+for i in w:
+    l1.append(i.lower())#去掉字符后小写化放入列表
