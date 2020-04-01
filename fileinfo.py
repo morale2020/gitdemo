61b4599729327bcf4c90837593ccbe8a5864be50
morale <morale@gmail.com>
2020-04-01 23:29:46 +0800

    update

--- fileinfo.py
+++ fileinfo.py
@@ -26,3 +26,19 @@ line = ff.replace('.',' ').replace('!',' ').replace(',',' ').replace('?',' ').re
 w = line.split()
 for i in w:
     l1.append(i.lower())#去掉字符后小写化放入列表
+
+#读取英文停用词表
+f1 = open('英文停用词表.txt')
+stopword = f1.readlines()
+l2 = []
+for i in stopword:
+    i = i.strip()
+    l2.append(i)#将停用词表放入列表
+
+#去掉停用词后的单词放入列表
+l3 = []
+for i in l1:
+    if i not in l2:
+        l3.append(i)
+    else:
+        continue
